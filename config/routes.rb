8393317c545f5954938dfbde52a256af75fdd1b0
Wen-Tien Chang <ihower@gmail.com>
2017-03-29 17:19:59 +0800

    Add jQuery basic demo code

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/jquery-1-2" => "pages#jquery_1_2"
+  get "/jquery-1-3" => "pages#jquery_1_3"
+  get "/jquery-1-4" => "pages#jquery_1_4"
+  get "/jquery-1-5" => "pages#jquery_1_5"
+  get "/jquery-1-6" => "pages#jquery_1_6"
+
+  root "pages#jquery_1_2"
+
 end
