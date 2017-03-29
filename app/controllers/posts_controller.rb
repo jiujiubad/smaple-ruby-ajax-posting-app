7ab4b74bcd0c7af30bef45d835dd3694069a23a4
Wen-Tien Chang <ihower@gmail.com>
2017-03-29 20:19:56 +0800

    User can +1, -1 post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,23 @@ class PostsController < ApplicationController
     @posts = Post.order("id DESC").all
   end
 
+  def like
+    @post = Post.find(params[:id])
+    unless @post.find_like(current_user)
+      Like.create( :user => current_user, :post => @post)
+    end
+
+    redirect_to posts_path
+  end
+
+  def unlike
+    @post = Post.find(params[:id])
+    like = @post.find_like(current_user)
+    like.destroy
+
+    redirect_to posts_path
+  end
+
   def create
     @post = Post.new(post_params)
     @post.user = current_user
