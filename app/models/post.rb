b912248b76c8093f3a013e89c4075c540507f0ef
Wen-Tien Chang <ihower@gmail.com>
2017-03-29 20:34:54 +0800

    Check current_user

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,11 @@ class Post < ApplicationRecord
   has_many :liked_users, :through => :likes, :source => :user
 
   def find_like(user)
-    self.likes.where( :user_id => user.id ).first
+    if user
+      self.likes.where( :user_id => user.id ).first
+    else
+      nil
+    end
   end
 
 end
