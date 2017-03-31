144e0bdbe9075acd0464331a28f6d6eaadcca945
Wen-Tien Chang <ihower@gmail.com>
2017-03-31 14:51:01 +0800

    Rename pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/jquery-1-2" => "pages#jquery_1_2"
-  get "/jquery-1-3" => "pages#jquery_1_3"
-  get "/jquery-1-4" => "pages#jquery_1_4"
-  get "/jquery-1-5" => "pages#jquery_1_5"
-  get "/jquery-1-6" => "pages#jquery_1_6"
+  get "/jquery-1" => "pages#jquery_1"
+  get "/jquery-2" => "pages#jquery_2"
+  get "/jquery-3" => "pages#jquery_3"
+  get "/jquery-4" => "pages#jquery_4"
+  get "/jquery-5" => "pages#jquery_5"
 
-  root "posts#index"
+  root  "pages#jquery_1"
+  #root "posts#index"
 
 end
